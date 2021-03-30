23f8558e274a2363ef1acc3bb0ff1755468260bc
fangchunxi1999 <fangchunxi19990113@gmail.com>
2021-03-31 00:39:21 +0700

    Change to use Spreadsheet onFormSubmit Trigger

--- Trigger.ts
+++ Trigger.ts
@@ -1,26 +1,59 @@
-function onOpen(e) {
+function onOpen(e: GoogleAppsScript.Events.SheetsOnOpen) {
     setupSpreadsheetAppUI();
 }
 
-function formOnSummit(e) {
-    //TODO
+function sheetOnSummit(e: GoogleAppsScript.Events.SheetsOnFormSubmit) {
+    var formatInfos = [
+        {
+            format: 'yyyy"/"mm"/"dd" "hh":"mm":"ss',
+            col: 1,
+        },
+        {
+            format: "#,##0",
+            col: 5,
+        },
+    ];
+    setFormat(e.range, formatInfos);
+
+    if (
+        e.range.getSheet().getSheetId().toString() !==
+        getConfig("CURRENT_LINKED_SHEET_ID")
+    ) {
+        return;
+    }
+    //Todo
 
     //debug
     //getCellFormat_DEBUG(e.range);
 }
 
-function addFormOnSubmitTrigger(
-    form: GoogleAppsScript.Forms.Form,
-    triggerFnName = "formOnSummit"
+function formOnSubmit(e: GoogleAppsScript.Events.FormsOnFormSubmit) {}
+
+function hasSheetOnSubmitTrigger(): boolean {
+    for (const trigger of ScriptApp.getProjectTriggers()) {
+        if (
+            trigger.getEventType() == ScriptApp.EventType.ON_FORM_SUBMIT &&
+            trigger.getTriggerSource() == ScriptApp.TriggerSource.SPREADSHEETS
+        ) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function addSheetOnSubmitTrigger(
+    spreadSheet: GoogleAppsScript.Spreadsheet.Spreadsheet,
+    triggerFnName = "sheetOnSummit"
 ): GoogleAppsScript.Script.Trigger {
     return ScriptApp.newTrigger(triggerFnName)
-        .forForm(form)
+        .forSpreadsheet(spreadSheet)
         .onFormSubmit()
         .create();
 }
 
 function delOldOnFormSubmitTriggers(currentTriggerID: string) {
-    var triggers = ScriptApp.getProjectTriggers();
+    const triggers = ScriptApp.getProjectTriggers();
     for (let trigger of triggers) {
         if (trigger.getEventType() == ScriptApp.EventType.ON_FORM_SUBMIT) {
             if (trigger.getUniqueId() != currentTriggerID) {
